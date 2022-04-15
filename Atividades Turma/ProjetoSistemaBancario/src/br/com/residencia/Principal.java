7727fe8090470504ae30759bd59a8c83ec447204
Geisalves64 <geisalves@gmail.com>
2022-04-14 21:33:20 -0300

    nova atualização

--- Atividades Turma/ProjetoSistemaBancario/src/br/com/residencia/Principal.java
+++ Atividades Turma/ProjetoSistemaBancario/src/br/com/residencia/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	public static void main(String[] args) throws IOException {
 		
 		//Como chamar o método leitor da classe LeituraEscrita
-		LeituraEscrita.leitor("informacoes_bancarias.txt");
+		LeituraEscrita.leitor("entrada.txt");
 		
 		//Como chamar o método escritor da classe LeituraEscrita
 		LeituraEscrita.escritor("./");
