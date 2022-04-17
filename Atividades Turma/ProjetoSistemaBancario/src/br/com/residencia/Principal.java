b708d74dbef298c07d0f74fb5e6dffa822547aec
Geisalves64 <geisalves@gmail.com>
2022-04-17 19:37:12 -0300

    Novas atualizações nos métodos

--- Atividades Turma/ProjetoSistemaBancario/src/br/com/residencia/Principal.java
+++ Atividades Turma/ProjetoSistemaBancario/src/br/com/residencia/Principal.java
@@ -2,6 +2,8 @@ package br.com.residencia;
 
 import java.io.IOException;
 
+import br.com.residencia.model.Conta;
+
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
@@ -12,6 +14,26 @@ public class Principal {
 		//Como chamar o método escritor da classe LeituraEscrita
 		LeituraEscrita.escritor("./");
 
-	}
+		System.out.println("\nMetodo transferir, depositar e sacar\n");
+
+			Conta minhaConta = new Conta(null, null, null);
+			minhaConta.titular = "Juvenal";
+			minhaConta.saldo = 1200;
+			minhaConta.numero = 321;
+
+			Conta suaConta = new Conta(null, null, null);
+			suaConta.titular = "Coringa";
+			suaConta.saldo = 30000;
+			suaConta.numero = 666;
+			minhaConta.transferir(suaConta, 1000);
+			minhaConta.depositar(600);
+			minhaConta.sacar(415);
+			
+			System.out.println(minhaConta.titular + " " + minhaConta.saldo);
+			System.out.println(suaConta.titular + " " + suaConta.saldo);
+		
+			
+		}
+
 
 }
